2fd295cf3aa637ba6fbb2bfd2f02401951e9c8da
liam <lamtruong@panicbear.consulting>
2023-09-24 05:49:27 +0700

    build

--- js/home.js
+++ js/home.js
@@ -1,10 +1,11 @@
-import Lenis from '@studio-freight/lenis'
-
-const lenis = new Lenis()
-
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
+setTimeout(() => {
+  document.querySelector('.loader__wrap').classList.add('loaded')
+}, 300);
+document.querySelector('[data-popup="open"]').addEventListener('click', function(e) {
+  e.preventDefault();
+  document.querySelector('.popup').classList.add('active')
+})
+document.querySelector('[data-popup="close"]').addEventListener('click', function(e) {
+  e.preventDefault();
+  document.querySelector('.popup').classList.remove('active')
+})
